785cd78e206ad88215699a30294002c224920ff4
hambalee <18182920+hambalee@users.noreply.github.com>
2023-11-14 10:09:20 +0700

    not found page
    `http://localhost:3000/products/1/reviews/1001`

--- app/products/[productId]/reviews/[reviewId]/page.tsx
+++ app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,11 +1,18 @@
+import { notFound } from "next/navigation";
+
 export default function ReviewDetail({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) {
+  if (parseInt(params.reviewId) > 1000) {
+    return notFound();
+  }
   return (
     <>
-      <h1>Review {params.reviewId} for product {params.productId}</h1>
+      <h1>
+        Review {params.reviewId} for product {params.productId}
+      </h1>
     </>
   );
 }
